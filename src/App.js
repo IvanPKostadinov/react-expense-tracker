6ebc242ec1bfcb8703bbd6df1b846fd649cf9ebe
IvanPKostadinov <ikostadinov@melontech.com>
2022-02-25 14:15:02 +0200

    added first component - ExpenseItem.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
+    // This code is called JSX
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>02.03.2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+      </div>
+      <div>$299.99</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
