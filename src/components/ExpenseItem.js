83ef60db4da880dbd335c31e5c938e5f12493757
IvanPKostadinov <ikostadinov@melontech.com>
2022-02-25 16:03:17 +0200

    added dynamic data to the JSC code in ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 299.99;
+
   return (
-    <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
       </div>
-      <div className="expense-item__price">$299.99</div>
+      <div className='expense-item__price'>$ {expenseAmount}</div>
     </div>
   );
 }
