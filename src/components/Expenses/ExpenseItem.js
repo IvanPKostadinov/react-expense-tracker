114f0eb0c48d87d102a0c3d96b6fb72baced7d79
IvanPKostadinov <ikostadinov@melontech.com>
2022-03-09 18:00:25 +0200

    did some cleanup work

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,18 @@
-import React, { useState } from 'react'; // technically we don't need this now
+import React from 'react'; // technically we don't need this now
 
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] =  useState(props.title); // const [currentStateValue, setStateValueFunction] = useState(initialValue);
-
-  const clickHandler = () => {
-    setTitle('Updated!'); // the Component function is called again with this value of the State
-    console.log(title);
-  };
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       {/* <div>{props.date.toISOString()}</div> */}
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className='expense-item__price'>$ {props.amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
