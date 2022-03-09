737c509a13be6a148b9350bd729a7f121081c94d
IvanPKostadinov <ikostadinov@melontech.com>
2022-03-09 19:15:08 +0200

    outputting conditional content in Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,9 +13,22 @@ const Expenses = (props) => {
   };
 
   // .filter() returns new Array, doesn't edit the original Array
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  })
+  });
+
+  // we can store JSX code in variables:
+  let expensesContent = <p>No Expenses Found!</p>;
+
+  if(filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />));
+  }
 
   return (
     <div>
@@ -24,14 +37,33 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {/* OPTION 1: */}
+        {/* {filteredExpenses.length === 0 
+          ? <p>No Expenses Found!</p>
+          : filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+        ))} */}
+
+        {/* OPTION 2: */}
+        {/* && returns the 2nd if the 1st is "true" */}
+        {/* {filteredExpenses.length === 0 && <p>No Expenses Found!</p>}
+        {filteredExpenses.length > 0 && 
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />))
+        } */}
+
+        {/* OPTION 3: */}
+        {expensesContent}
       </Card>
     </div>
   );
