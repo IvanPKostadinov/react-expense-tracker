1bc60020eb6c1c691f125f69a1420773e1597b96
IvanPKostadinov <ikostadinov@melontech.com>
2022-03-10 11:36:56 +0200

    exported the ExpensesList to a separate file and added conditional Return statement in it

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'; // technically we don't need this now
 
 import './Expenses.css';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -12,23 +12,11 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  // we can store JSX code in variables:
   // .filter() returns new Array, doesn't edit the original Array
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-
-  // we can store JSX code in variables:
-  let expensesContent = <p>No Expenses Found!</p>;
-
-  if(filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />));
-  }
 
   return (
     <div>
@@ -63,7 +51,8 @@ const Expenses = (props) => {
         } */}
 
         {/* OPTION 3: */}
-        {expensesContent}
+        {/* {expensesContent} */}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
